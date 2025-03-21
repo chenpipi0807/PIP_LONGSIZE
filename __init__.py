145e4970bb9e41de0f0298aafd3b8aabc7f53f0d
chenpipi0807 <144689950+chenpipi0807@users.noreply.github.com>
2025-03-21 15:41:49 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
-from .PIP_longsize import PIP_longsize
+from .PIP_longsize import PIP_longsize, PIP_ProportionalCrop
 
 NODE_CLASS_MAPPINGS = {
-    "PIP_longsize": PIP_longsize
+    "PIP_longsize": PIP_longsize,
+    "PIP_ProportionalCrop": PIP_ProportionalCrop
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "PIP_longsize": "PIP 长边调整"
+    "PIP_longsize": "PIP 长边调整",
+    "PIP_ProportionalCrop": "PIP 等比例裁切"
 }
