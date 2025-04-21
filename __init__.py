61f609ba35a5910d92767430bb15f19dfc128b65
chenpipi0807 <144689950+chenpipi0807@users.noreply.github.com>
2025-04-21 17:28:37 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 from .PIP_longsize import PIP_longsize, PIP_ProportionalCrop
+from .PIP_integer_calculator import PIP_IntegerCalculator
 
 NODE_CLASS_MAPPINGS = {
     "PIP_longsize": PIP_longsize,
-    "PIP_ProportionalCrop": PIP_ProportionalCrop
+    "PIP_ProportionalCrop": PIP_ProportionalCrop,
+    "PIP_IntegerCalculator": PIP_IntegerCalculator
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "PIP_longsize": "PIP 长边调整",
-    "PIP_ProportionalCrop": "PIP 等比例裁切"
+    "PIP_ProportionalCrop": "PIP 等比例裁切",
+    "PIP_IntegerCalculator": "PIP 整数计算"
 }
